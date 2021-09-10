cdd60ab2ba69b07400f754e013a1bd42c7891f66
Winicius Souza <winiciussouzadev@gmail.com>
2021-09-10 19:46:59 -0300

    Update Icon

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -8,7 +8,7 @@ export function Profile() {
 
     return (
         <div className={styles.profileContainer}>
-            <img src="https://w7.pngwing.com/pngs/87/12/png-transparent-game-controller-internet-addiction-disorder-digital-detox-eastlink-games-icon-miscellaneous-game-logo.png" alt="Winicius" />
+            <img src="https://produtive.me/wp-content/uploads/2020/05/cropped-Produtive_N_Logo.png" alt="Winicius" />
             <div>
                 <strong>Winicius Souza</strong>
                 <p><img src="icons/level.svg" alt="level" />Level {level}</p>
