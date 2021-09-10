a0fab84ff391d4e4e7a6a1dc2c8f752a956dbfc4
Winicius Souza <winiciussouzadev@gmail.com>
2021-09-10 19:40:04 -0300

    Update

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,13 +1,14 @@
 import { useContext } from 'react';
 import { ChallengeContext } from '../contexts/ChallengeContext';
-import styles from '../styles/components/Profile.module.css'
+import styles from '../styles/components/Profile.module.css';
+
 export function Profile() {
 
     const { level }  = useContext(ChallengeContext);
 
     return (
         <div className={styles.profileContainer}>
-            <img src="https://github.com/WiniciusNvoip.png" alt="Marco Antonio" />
+            <img src="https://w7.pngwing.com/pngs/87/12/png-transparent-game-controller-internet-addiction-disorder-digital-detox-eastlink-games-icon-miscellaneous-game-logo.png" alt="Winicius" />
             <div>
                 <strong>Winicius Souza</strong>
                 <p><img src="icons/level.svg" alt="level" />Level {level}</p>
